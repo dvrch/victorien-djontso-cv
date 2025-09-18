d13109cd08d52e085232542fd320b9bdd5f35bd3
k! <dvrchipro+klwp@gmail.com>
2025-09-18 12:05:08 +0200

    style: make markdown images responsive

--- svelte-3d-viewer/src/routes/[slug]/+page.svelte
+++ svelte-3d-viewer/src/routes/[slug]/+page.svelte
@@ -14,4 +14,11 @@
     padding: 2rem;
     line-height: 1.6;
   }
+
+  :global(.markdown-body img) {
+    max-width: 100%;
+    height: auto;
+    display: block;
+    margin: 1rem auto;
+  }
 </style>
