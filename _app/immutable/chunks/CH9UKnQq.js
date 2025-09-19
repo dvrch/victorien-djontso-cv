b92a61ef0d2acfef31ad53ce5d3b51c74e69d64a
dvrch <dvrch@users.noreply.github.com>
2025-09-19 17:01:00 +0000

    deploy: 7b4279eb2bf6ffc7782a1e16f30c00030b1a0d56

---
+++ _app/immutable/chunks/CH9UKnQq.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1uggytt)==null?void 0:s.base)??"/victorien-djontso-cv";var t;const a=((t=globalThis.__sveltekit_1uggytt)==null?void 0:t.assets)??e;export{a,e as b};
