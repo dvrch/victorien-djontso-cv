242775e482322cc3665cde79517901436d164fa4
dvrch <dvrch@users.noreply.github.com>
2025-10-01 18:50:55 +0000

    deploy: d7c17d3c0ebb65f2c4349cee018ad237937a7a1b

---
+++ _app/immutable/chunks/D9id0HZL.js
@@ -0,0 +1,6 @@
+const e=`# Intégration 3D
+
+Cette page intègre la visionneuse 3D Svelte.
+
+<iframe src="/3d-viewer-embed" width="100%" height="600" style="border:0; max-width: 100%;"></iframe>
+`;export{e as default};
