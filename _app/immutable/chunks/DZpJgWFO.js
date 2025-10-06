45fb4d5439cef0ae597f8cb204338568fefc163f
dvrch <dvrch@users.noreply.github.com>
2025-10-06 17:56:48 +0000

    deploy: e20da6cf83694878521eaf9392553883eb770d9b

---
+++ _app/immutable/chunks/DZpJgWFO.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1j28q1q)==null?void 0:s.base)??"/victorien-djontso-cv";var t;const a=((t=globalThis.__sveltekit_1j28q1q)==null?void 0:t.assets)??e;export{a,e as b};
---
+++ _app/immutable/entry/start.CzT2Tfg3.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/C01wc98b.js";export{o as load_css,r as start};
